5664ff33c88eff0f9336c3d8894f5e1183a686c8
Marwan <muhdmarwan07@gmail.com>
2019-09-16 16:56:12 +0800

    4:55-16/9/19

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,13 +18,34 @@ class ProductController extends Controller
     public function save_product(Request $request)
     {
     	$data=array();
-    	$data['product_id']=$request->product_id;
     	$data['product_name']=$request->product_name;
+        $data['category_id']=$request->category_id;
+        $data['manufacture_id']=$request->manufacture_id;
     	$data['product_description']=$request->product_description;
     	$data['publication_status']=$request->publication_status;
+        $data['product_price']=$request->product_price;
+        $data['product_size']=$request->product_size;
+        $data['product_color']=$request->product_color;
 
-    	DB::table('tbl_product')->insert($data);
-    	Session::put('message','Product added Sucessfully ');
-    	return Redirect::to('/add-product');
+        $image=$request->file('product_image');
+        if($image)
+        {
+            $image_name=str_random(20);
+            $ext=strtolower($image->getClientOriginalExtension());
+            $image_full_name=$image_name.'.'.$ext;
+            $upload_path='image/';
+            $image_url=$upload_path.$image_full_name;
+            $success=$image->move($upload_path,$image_full_name);
+            if($success)
+                $data['product_image']=$image_url;
+            DB::table('tbl_products')->insert($data);
+        Session::put('message','Product added Sucessfully ');
+        return Redirect::to('/add-product');
+        }
+
+    	$data['product_image']='';
+            DB::table('tbl_products')->insert($data);
+        Session::put('message','Product added Sucessfully without image ');
+        return Redirect::to('/add-product');
     }
 }
--- resources/views/admin/add_product.blade.php
+++ resources/views/admin/add_product.blade.php
@@ -40,7 +40,7 @@
                             <div class="review-tab-pro-inner">
                                 <ul id="myTab3" class="tab-review-design">
                                     <li class="active"><a href="#description"><i class="icon nalika-edit" aria-hidden="true"></i> Add Product</a></li>
-                                    <li><a href="#reviews"><i class="icon nalika-picture" aria-hidden="true"></i> Pictures</a></li>
+                                    <!-- <li><a href="#reviews"><i class="icon nalika-picture" aria-hidden="true"></i> Pictures</a></li> -->
                                     <!-- <li><a href="#INFORMATION"><i class="icon nalika-chat" aria-hidden="true"></i> Review</a></li> -->
                                 </ul>
                                 <p class="alert-success">
@@ -59,7 +59,7 @@
 
                                 <div id="myTabContent" class="tab-content custom-product-edit">
                                     <div class="product-tab-list tab-pane fade active in" id="description">
-                                     <form action="{{ url('/save-manufacture') }}" method="post">
+                                     <form action="{{ url('/save-product') }}" method="post" enctype="multipart/from-data">
                                             {{csrf_field()}}
                                         <div class="row">
                                             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
@@ -70,21 +70,23 @@
                                                     </div>
                                                     <div class="input-group mg-b-pro-edt" >
                                                         <span class="input-group-addon"></i></span>
-                                                                  <select class="form-control" id="sel1">
-                                                                   <option>Manufacture Name</option>
-                                                                    <option>2</option>
-                                                                    <option>3</option>
-                                                                    <option>4</option>
+                                                                  <select class="form-control" id="sel1" name="category_id">
+                                                                    <option>Select Category</option>
+
+                                                                    <?php
+                                                                        $all_published_category=DB::table('tbl_category')
+                                                                                                ->where('publication_status',1)
+                                                                                                ->get();
+                                                                        foreach ($all_published_category as $v_category) {?>
+
+                                                                   <option value="{{$v_category->category_name}}">{{$v_category->category_name}}</option>
+                                                                   <?php }?>
                                                                   </select>
                                                     </div>
                                                     <div class="input-group mg-b-pro-edt">
                                                         <span class="input-group-addon"><i class="fa fa-usd" aria-hidden="true"></i></span>
                                                         <input type="text" class="form-control" name="product_price" placeholder="Product Price" required="">
                                                     </div>
-                                                    <!-- <div class="input-group mg-b-pro-edt">
-                                                        <span class="input-group-addon"><i class="icon nalika-new-file" aria-hidden="true"></i></span>
-                                                        <input type="text" class="form-control" placeholder="Tax">
-                                                    </div> -->
                                                     <div class="input-group mg-b-pro-edt">
                                                         <span class="input-group-addon"><i class="icon nalika-favorites" aria-hidden="true"></i></span>
                                                         <input type="text" class="form-control" name="product_size" placeholder="Product Size" required="">
@@ -97,43 +99,48 @@
 
                                                     <div class="input-group mg-b-pro-edt">
                                                         <span class="input-group-addon"><i class="icon nalika-favorites" aria-hidden="true"></i></span>
-                                                        <input type="text" class="form-control" placeholder="Quantity">
+                                                        <input type="text" class="form-control" name="product_quantity" placeholder="Product Quantity" required="">
                                                     </div> 
+
+                                                    <div class>
+                                                        <input type="checkbox" name="publication_status" value="1"><font color="white">Publication Status</font>
+                                                    </div>
                                                 </div>
                                             </div>
                                             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                                                 <div class="review-content-section">
-                                                    <!-- <div class="input-group mg-b-pro-edt">
-                                                        <span class="input-group-addon"><i class="icon nalika-user" aria-hidden="true"></i></span>
-                                                        <input type="text" class="form-control" placeholder="Last Name">
-                                                        </div>  -->
                                                     <div class="input-group mg-b-pro-edt">
                                                         <span class="input-group-addon"><i class="icon nalika-edit" aria-hidden="true"></i></span>
-                                                        <input type="text" class="form-control" placeholder="Product Description" required="">
-                                                    </div>
-                                                    <!-- <div class="input-group mg-b-pro-edt">
-                                                        <span class="input-group-addon"><i class="fa fa-usd" aria-hidden="true"></i></span>
-                                                        <input type="text" class="form-control" placeholder="Sale Price">
+                                                        <input type="text" class="form-control" name="product_description" placeholder="Product Description" required="">
                                                     </div>
                                                     <div class="input-group mg-b-pro-edt">
-                                                        <span class="input-group-addon"><i class="icon nalika-like" aria-hidden="true"></i></span>
-                                                        <input type="text" class="form-control" placeholder="Category">
-                                                    </div> -->
-                                                         <select name="select" class="form-control pro-edt-select form-control-primary">
-															<option value="opt1">Product Category</option>
-															<option value="opt2">2</option>
-															<option value="opt3">3</option>
-															<option value="opt4">4</option>
-															<option value="opt5">5</option>
-															<option value="opt6">6</option>
+                                                        <span class="input-group-addon"><i class="icon nalika-edit" aria-hidden="true"></i></span>
+                                                        <label class="control-label" for="fileInput"><font color=white>Product Image</font></label>
+                                                        <div class="controls">
+                                                            <font color=white><input type="file" class="input-file uniform_on" name="product_image" id="fileInput" required=""></font>
+                                                        </div>
+                                                    </div>
+                                                         <select name="manufacture_id" class="form-control pro-edt-select form-control-primary">
+															<option value="opt1">Product Manufacture</option>
+
+                                                            <?php
+                                                                        $all_published_manufacture=DB::table('tbl_manufacture')
+                                                                                                ->where('publication_status',1)
+                                                                                                ->get();
+                                                                        foreach ($all_published_manufacture as $v_manufacture) {?>
+
+                                                                   <option value="{{$v_manufacture->manufacture_name}}">{{$v_manufacture->manufacture_name}}</option>
+                                                                   <?php }?>
 														 </select> 
-                                                </div> 
+
+                                                </div>
+
                                             </div>
                                         </div>
                                         <div class="row">
                                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                                 <div class="text-center custom-pro-edt-ds">
-                                                    <button type="button" class="btn btn-ctl-bt waves-effect waves-light m-r-10">Add Product
+                                                    <button type="submit" class="btn btn-ctl-bt waves-effect waves-light m-r-10">Add Product
 														</button>
                                                     <button type="button" class="btn btn-ctl-bt waves-effect waves-light">Discard
 														</button>
@@ -141,7 +148,7 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="product-tab-list tab-pane fade" id="reviews">
+                                    <!-- <div class="product-tab-list tab-pane fade" id="reviews">
                                         <div class="row">
                                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                                 <div class="review-content-section">
@@ -193,8 +200,8 @@
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                    </div>
-                                                    <div class="row">
+                                                    </div> -->
+                                                    <!-- <div class="row">
                                                         <div class="col-lg-4">
                                                             <div class="pro-edt-img">
                                                                 <img src="{{asset('backend/img/new-product/6-small.jpg')}}" alt="" />
@@ -242,8 +249,8 @@
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                    </div>
-                                                    <div class="row">
+                                                    </div> -->
+                                                    <!-- <div class="row">
                                                         <div class="col-lg-4">
                                                             <div class="pro-edt-img mg-b-0">
                                                                 <img src="{{asset('backend/img/new-product/7-small.jpg')}}" alt="" />
@@ -295,8 +302,8 @@
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="product-tab-list tab-pane fade" id="INFORMATION">
+                                    </div> -->
+                                    <!-- <div class="product-tab-list tab-pane fade" id="INFORMATION">
                                         <div class="row">
                                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                                 <div class="review-content-section">
@@ -351,7 +358,7 @@
                                                         <div class="form-group review-pro-edt mg-b-0-pt">
                                                             <button type="submit" class="btn btn-ctl-bt waves-effect waves-light">Submit
 																</button>
-                                                        </div>
+                                                        </div> -->
                                                     </div>
                                                 </div>
                                             </div>
